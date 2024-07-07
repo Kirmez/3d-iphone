689b69af704ca9f70cfae9bdcfc832583baea34b
Kirill Mezentsev <kirmez02@gmail.com>
2024-07-07 18:31:08 +0200

    added carousel

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
+import Hero from "./components/Hero";
+import Highlights from "./components/Highlights";
+import Model from "./components/Model";
+import Navbar from "./components/Navbar";
+
 const App = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <main className="bg-black">
+      <Navbar />
+      <Hero />
+      <Highlights />
+      <Model 
+        
+      />
+    </main>
+  );
 };
 
 export default App;
